bacac9d0b69c4a26ff33a659d4442e72b768576b
Brian Ramirez <bramirez@umass.edu>
2017-02-10 20:22:07 -0500

    migrations, and layout changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,6 @@ class Movie < ApplicationRecord
 
   has_attached_file :image, styles: { medium: "400x600#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  has_many :reviews
 end
